b002a71d32111c691b8f8f9dbf706b1a53dc08b5
Mewes Kochheim <mewes@kochheim.de>
2013-09-29 22:20:25 +0200

    semi finished build script

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## AdvancedBrewing @VERSION@
+## AdvancedBrewing 0.1
 
--- common/advancedbrewing/Reference.java
+++ common/advancedbrewing/Reference.java
@@ -3,7 +3,7 @@ package advancedbrewing;
 public class Reference {
     public static final String MOD_ID = "AdvancedBrewing";
     public static final String MOD_NAME = "Advanced Brewing";
-    public static final String VERSION_NUMBER = "@VERSION@ (build @BUILD_NUMBER@)";
+    public static final String VERSION_NUMBER = "@VERSION@.@BUILD_NUMBER@";
     public static final String CHANNEL_NAME = MOD_ID;
     public static final String DEPENDENCIES = "required-after:Forge@[9.10.0.800,)";
     public static final String CLIENT_PROXY_CLASS = "advancedbrewing.proxy.ProxyClient";
